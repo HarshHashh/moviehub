0e32a6ffba1295331feddb9034724080ddf12942
HarshHashh <158750193+HarshHashh@users.noreply.github.com>
2026-05-13 10:55:13 +0530

    API

---
+++ src/app/api/apiConfig.ts
@@ -0,0 +1,14 @@
+// apiConfig.ts
+export const API_BASE_URL = "https://api.hicine.info";
+
+
+export const formator = "https://hicine-formator-one.vercel.app/type";
+
+
+export const endpoint = 'https://sgp.cloud.appwrite.io/v1'; 
+export const projectId = '695c9c3400040dad154b';
+export const databaseId = '695ca222000ca9fa6fec';
+export const collectionId = 'trending';
+
+export const apikey = 'd562dd4725d2ddc02dffc7b0d76796ac';
+export const baseurldb = "https://api.themoviedb.org/3";
